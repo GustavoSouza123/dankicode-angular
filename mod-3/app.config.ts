ccc533873f45909c955c766a5526d37eedfc587b
Gustavo Souza <gustavoelia7@gmail.com>
2025-02-14 11:25:38 -0300

    services module folder

--- mod-3/app.config.ts
+++ mod-3/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { UserService } from './user.service';
+import { LoggingService } from './logging.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    UserService,
+    LoggingService,
   ],
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NewUserComponent } from './new-user/new-user.component';
-import { UserComponent } from './user/user.component';
-import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewUserComponent, UserComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [],
 })
-export class AppComponent implements OnInit {
-  users!: { name: string; status: string }[];
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    this.users = this.userService.users;
-  }
-}
+export class AppComponent {}
